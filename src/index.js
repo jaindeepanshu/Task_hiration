c87c277299c77607fef6dc6458b69f182fc140c1
deepanshu jain <jaindeepansh17@gmail.com>
2018-11-26 14:12:01 +0530

    index modified
    as per requirement

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import App from "./new.js";
 import "./styles.css";
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <h2>Lets create some miracle!</h2>
     </div>
   );
-}
+}*/
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
